8111f3d2f6924a314ce676f98d9dff2a3e8a6b55
gabriel-piangers <piangersgabriel@gmail.com>
2025-10-13 09:21:59 -0300

    fix: import SearchForm

--- front-end/src/pages/SearchPage.jsx
+++ front-end/src/pages/SearchPage.jsx
@@ -1,5 +1,5 @@
 import { Header } from "../components/Header";
-import { SearchForm } from "../components/searchForm";
+import { SearchForm } from "../components/SearchForm";
 import {
   printReais,
   convertUrlsToLinks,
